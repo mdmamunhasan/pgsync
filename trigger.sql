768261820a53d50077604e1e02289b496b73988a
mamun <mamunthecoder@gmail.com>
2017-08-07 03:02:20 +0600

    trigger update

--- trigger.sql
+++ trigger.sql
@@ -15,6 +15,12 @@ $$ LANGUAGE plpgsql;
 DROP TRIGGER commissions_notify_update ON commissions;
 CREATE TRIGGER commissions_notify_update AFTER UPDATE ON commissions FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
+DROP TRIGGER commissions_notify_update ON commissions;
+CREATE TRIGGER commissions_notify_insert AFTER INSERT ON commissions FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+
+DROP TRIGGER commissions_notify_update ON commissions;
+CREATE TRIGGER commissions_notify_delete AFTER DELETE ON commissions FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+
 DROP TRIGGER retailers_notify_insert ON retailers;
 CREATE TRIGGER retailers_notify_insert AFTER INSERT ON retailers FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
