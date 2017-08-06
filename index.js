d6ae5335e6206bf4c8770f8af43fc6c1da866b23
mamun <mamunthecoder@gmail.com>
2017-08-07 02:26:07 +0600

    spark processor

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var pg = require('pg');
 
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+    accessKeyId: "AKIAJFDJF2NLFLHTWFPA",
+    secretAccessKey: "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",
+    region: "ap-southeast-1"
+});
+
+var kinesis = new AWS.Kinesis({apiVersion: '2013-12-02'});
+
 pg.connect("postgres://postgres:root@127.0.0.1:5432/db_local", function (err, client) {
     if (err) {
         console.log(err);
@@ -10,6 +20,10 @@ pg.connect("postgres://postgres:root@127.0.0.1:5432/db_local", function (err, cl
             console.log("*========*");
             Object.keys(pl).forEach(function (key) {
                 console.log(key, pl[key]);
+            });
+            kinesis.addTagsToStream("ok", function (err, data) {
+                if (err) console.log(err, err.stack); // an error occurred
+                else     console.log(data);           // successful response
             });
             console.log("-========-");
         }
