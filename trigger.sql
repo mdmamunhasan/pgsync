c7d2a1a5fddb7a464a24cd8e9c7fb6ee5ead0b2a
mamun <mamunthecoder@gmail.com>
2017-08-07 23:34:03 +0600

    trigger update

--- trigger.sql
+++ trigger.sql
@@ -104,70 +104,162 @@ CREATE TRIGGER discount_coupons_notify_update AFTER UPDATE ON discount_coupons F
 CREATE TRIGGER discount_coupons_notify_delete AFTER DELETE ON discount_coupons FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER discount_summary_notify_insert ON discount_summary;
+DROP TRIGGER discount_summary_notify_insert ON discount_summary;
+DROP TRIGGER discount_summary_notify_insert ON discount_summary;
+CREATE TRIGGER discount_summary_notify_insert AFTER INSERT ON discount_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER discount_summary_notify_insert AFTER INSERT ON discount_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER discount_summary_notify_insert AFTER INSERT ON discount_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER engagement_summary_notify_delete ON engagement_summary;
+DROP TRIGGER engagement_summary_notify_delete ON engagement_summary;
+DROP TRIGGER engagement_summary_notify_delete ON engagement_summary;
+CREATE TRIGGER engagement_summary_notify_delete AFTER DELETE ON engagement_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER engagement_summary_notify_delete AFTER DELETE ON engagement_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER engagement_summary_notify_delete AFTER DELETE ON engagement_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER failed_jobs_notify_update ON failed_jobs;
+DROP TRIGGER failed_jobs_notify_update ON failed_jobs;
+DROP TRIGGER failed_jobs_notify_update ON failed_jobs;
+CREATE TRIGGER failed_jobs_notify_update AFTER UPDATE ON failed_jobs FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER failed_jobs_notify_update AFTER UPDATE ON failed_jobs FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER failed_jobs_notify_update AFTER UPDATE ON failed_jobs FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER feature_addons_notify_insert ON feature_addons;
+DROP TRIGGER feature_addons_notify_insert ON feature_addons;
+DROP TRIGGER feature_addons_notify_insert ON feature_addons;
+CREATE TRIGGER feature_addons_notify_insert AFTER INSERT ON feature_addons FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER feature_addons_notify_insert AFTER INSERT ON feature_addons FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER feature_addons_notify_insert AFTER INSERT ON feature_addons FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER feedback_call_notify_delete ON feedback_call;
+DROP TRIGGER feedback_call_notify_delete ON feedback_call;
+DROP TRIGGER feedback_call_notify_delete ON feedback_call;
+CREATE TRIGGER feedback_call_notify_delete AFTER DELETE ON feedback_call FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER feedback_call_notify_delete AFTER DELETE ON feedback_call FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER feedback_call_notify_delete AFTER DELETE ON feedback_call FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER invoices_notify_insert ON invoices;
+DROP TRIGGER invoices_notify_insert ON invoices;
+DROP TRIGGER invoices_notify_insert ON invoices;
+CREATE TRIGGER invoices_notify_insert AFTER INSERT ON invoices FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER invoices_notify_insert AFTER INSERT ON invoices FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER invoices_notify_insert AFTER INSERT ON invoices FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER membership_history_notify_delete ON membership_history;
+DROP TRIGGER membership_history_notify_delete ON membership_history;
+DROP TRIGGER membership_history_notify_delete ON membership_history;
+CREATE TRIGGER membership_history_notify_delete AFTER DELETE ON membership_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER membership_history_notify_delete AFTER DELETE ON membership_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER membership_history_notify_delete AFTER DELETE ON membership_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER messages_notify_update ON messages;
+DROP TRIGGER messages_notify_update ON messages;
+DROP TRIGGER messages_notify_update ON messages;
+CREATE TRIGGER messages_notify_update AFTER UPDATE ON messages FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER messages_notify_update AFTER UPDATE ON messages FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER messages_notify_update AFTER UPDATE ON messages FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER monthly_rgcheck_notify_insert ON monthly_rgcheck;
+DROP TRIGGER monthly_rgcheck_notify_insert ON monthly_rgcheck;
+DROP TRIGGER monthly_rgcheck_notify_insert ON monthly_rgcheck;
+CREATE TRIGGER monthly_rgcheck_notify_insert AFTER INSERT ON monthly_rgcheck FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER monthly_rgcheck_notify_insert AFTER INSERT ON monthly_rgcheck FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER monthly_rgcheck_notify_insert AFTER INSERT ON monthly_rgcheck FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER otp_notify_delete ON otp;
+DROP TRIGGER otp_notify_delete ON otp;
+DROP TRIGGER otp_notify_delete ON otp;
+CREATE TRIGGER otp_notify_delete AFTER DELETE ON otp FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER otp_notify_delete AFTER DELETE ON otp FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER otp_notify_delete AFTER DELETE ON otp FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER partnership_sms_notify_delete ON partnership_sms;
+DROP TRIGGER partnership_sms_notify_delete ON partnership_sms;
+DROP TRIGGER partnership_sms_notify_delete ON partnership_sms;
+CREATE TRIGGER partnership_sms_notify_delete AFTER DELETE ON partnership_sms FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER partnership_sms_notify_delete AFTER DELETE ON partnership_sms FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER partnership_sms_notify_delete AFTER DELETE ON partnership_sms FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER partnerships_notify_update ON partnerships;
+DROP TRIGGER partnerships_notify_update ON partnerships;
+DROP TRIGGER partnerships_notify_update ON partnerships;
+CREATE TRIGGER partnerships_notify_update AFTER UPDATE ON partnerships FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER partnerships_notify_update AFTER UPDATE ON partnerships FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER partnerships_notify_update AFTER UPDATE ON partnerships FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER payment_methods_notify_insert ON payment_methods;
+DROP TRIGGER payment_methods_notify_insert ON payment_methods;
+DROP TRIGGER payment_methods_notify_insert ON payment_methods;
+CREATE TRIGGER payment_methods_notify_insert AFTER INSERT ON payment_methods FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER payment_methods_notify_insert AFTER INSERT ON payment_methods FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER payment_methods_notify_insert AFTER INSERT ON payment_methods FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER profiles_notify_delete ON profiles;
+DROP TRIGGER profiles_notify_delete ON profiles;
+DROP TRIGGER profiles_notify_delete ON profiles;
+CREATE TRIGGER profiles_notify_delete AFTER DELETE ON profiles FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER profiles_notify_delete AFTER DELETE ON profiles FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER profiles_notify_delete AFTER DELETE ON profiles FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER registration_summary_notify_delete ON registration_summary;
+DROP TRIGGER registration_summary_notify_delete ON registration_summary;
+DROP TRIGGER registration_summary_notify_delete ON registration_summary;
+CREATE TRIGGER registration_summary_notify_delete AFTER DELETE ON registration_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER registration_summary_notify_delete AFTER DELETE ON registration_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER registration_summary_notify_delete AFTER DELETE ON registration_summary FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER retailer_invites_notify_delete ON retailer_invites;
+DROP TRIGGER retailer_invites_notify_delete ON retailer_invites;
+DROP TRIGGER retailer_invites_notify_delete ON retailer_invites;
+CREATE TRIGGER retailer_invites_notify_delete AFTER DELETE ON retailer_invites FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER retailer_invites_notify_delete AFTER DELETE ON retailer_invites FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER retailer_invites_notify_delete AFTER DELETE ON retailer_invites FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER transactions_notify_update ON transactions;
+DROP TRIGGER transactions_notify_update ON transactions;
+DROP TRIGGER transactions_notify_update ON transactions;
+CREATE TRIGGER transactions_notify_update AFTER UPDATE ON transactions FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER transactions_notify_update AFTER UPDATE ON transactions FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER transactions_notify_update AFTER UPDATE ON transactions FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER sms_log_notify_insert ON sms_log;
+DROP TRIGGER sms_log_notify_insert ON sms_log;
+DROP TRIGGER sms_log_notify_insert ON sms_log;
+CREATE TRIGGER sms_log_notify_insert AFTER INSERT ON sms_log FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER sms_log_notify_insert AFTER INSERT ON sms_log FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER sms_log_notify_insert AFTER INSERT ON sms_log FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER subscription_history_notify_delete ON subscription_history;
+DROP TRIGGER subscription_history_notify_delete ON subscription_history;
+DROP TRIGGER subscription_history_notify_delete ON subscription_history;
+CREATE TRIGGER subscription_history_notify_delete AFTER DELETE ON subscription_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER subscription_history_notify_delete AFTER DELETE ON subscription_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER subscription_history_notify_delete AFTER DELETE ON subscription_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER th_internal_members_notify_delete ON th_internal_members;
+DROP TRIGGER th_internal_members_notify_delete ON th_internal_members;
+DROP TRIGGER th_internal_members_notify_delete ON th_internal_members;
+CREATE TRIGGER th_internal_members_notify_delete AFTER DELETE ON th_internal_members FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER th_internal_members_notify_delete AFTER DELETE ON th_internal_members FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER th_internal_members_notify_delete AFTER DELETE ON th_internal_members FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER tonic_plus_history_notify_update ON tonic_plus_history;
+DROP TRIGGER tonic_plus_history_notify_update ON tonic_plus_history;
+DROP TRIGGER tonic_plus_history_notify_update ON tonic_plus_history;
+CREATE TRIGGER tonic_plus_history_notify_update AFTER UPDATE ON tonic_plus_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER tonic_plus_history_notify_update AFTER UPDATE ON tonic_plus_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER tonic_plus_history_notify_update AFTER UPDATE ON tonic_plus_history FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER usage_logs_notify_insert ON usage_logs;
+DROP TRIGGER usage_logs_notify_insert ON usage_logs;
+DROP TRIGGER usage_logs_notify_insert ON usage_logs;
+CREATE TRIGGER usage_logs_notify_insert AFTER INSERT ON usage_logs FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER usage_logs_notify_insert AFTER INSERT ON usage_logs FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 CREATE TRIGGER usage_logs_notify_insert AFTER INSERT ON usage_logs FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 DROP TRIGGER whitelist_notify_delete ON whitelist;
-CREATE TRIGGER whitelist_history_notify_delete AFTER DELETE ON whitelist FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+DROP TRIGGER whitelist_notify_delete ON whitelist;
+DROP TRIGGER whitelist_notify_delete ON whitelist;
+CREATE TRIGGER whitelist_notify_delete AFTER DELETE ON whitelist FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER whitelist_notify_delete AFTER DELETE ON whitelist FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+CREATE TRIGGER whitelist_notify_delete AFTER DELETE ON whitelist FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
