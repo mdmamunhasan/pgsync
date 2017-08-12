f64fa820af6b9a19236494f30e5e6c7226abe5f9
mamun <mamunthecoder@gmail.com>
2017-08-12 16:46:10 +0600

    config updated

--- trigger.js
+++ trigger.js
@@ -13,8 +13,8 @@ sync_table_list.forEach(function (table) {
     var drop_trigger = 'DROP TRIGGER $1 ON $2';
     var ctreate_trigger = "CREATE TRIGGER $1 AFTER INSERT ON $2 FOR EACH ROW EXECUTE PROCEDURE table_update_notify()";
     trigger_actions.forEach(function (trigger) {
-        var trigger_values = [table + 'notify_' + trigger, table];
-        client.query(trigger_values, drop_values, function (err, res) {
+        var trigger_values = [table + '_notify_' + trigger, table];
+        client.query(drop_trigger, trigger_values, function (err, res) {
             if (err) {
                 console.log(err.stack);
             } else {
