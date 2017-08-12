ece0c6c706efcd95340c3d42fe43fd70a777f43a
mamun <mamunthecoder@gmail.com>
2017-08-12 15:28:15 +0600

    config updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var pg = require('pg');
-
+var config = require('./config/config');
+const { Pool, Client } = require('pg');
 var AWS = require('aws-sdk');
 
 AWS.config.update({
@@ -10,24 +10,28 @@ AWS.config.update({
 
 var kinesis = new AWS.Kinesis({apiVersion: '2013-12-02'});
 
-pg.connect("postgres://postgres:root@127.0.0.1:5432/db_local", function (err, client) {
-    if (err) {
-        console.log(err);
-    }
-    client.on('notification', function (msg) {
-        if (msg.name === 'notification' && msg.channel === 'table_update') {
-            var pl = JSON.parse(msg.payload);
-            console.log("*========*");
-            Object.keys(pl).forEach(function (key) {
-                console.log(key, pl[key]);
-            });
-            kinesis.addTagsToStream("ok", function (err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else     console.log(data);           // successful response
-            });
-            console.log("-========-");
-        }
-    });
-
-    client.query("LISTEN table_update");
+const client = new Client({
+    user: config.db_user,
+    host: config.db_host,
+    database: config.db_name,
+    password: config.db_pass,
+    port: config.db_port
 });
+client.connect();
+
+client.on('notification', function (msg) {
+    if (msg.name === 'notification' && msg.channel === 'table_update') {
+        var pl = JSON.parse(msg.payload);
+        console.log("*========*");
+        Object.keys(pl).forEach(function (key) {
+            console.log(key, pl[key]);
+        });
+        kinesis.addTagsToStream("ok", function (err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+        });
+        console.log("-========-");
+    }
+});
+
+client.query("LISTEN table_update");
