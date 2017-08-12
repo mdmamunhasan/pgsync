52674662251b32a7b5a8b911dc2aa18cc7353c5b
mamun <mamunthecoder@gmail.com>
2017-08-12 18:08:17 +0600

    config updated

--- test/index.js
+++ test/index.js
@@ -1,22 +1,35 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var pg = require('pg');
+
+const Client = pg.Client;
+
+const client = new Client();
+client.connect();
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
     });
-  });
 });
 
-describe('User', function() {
-  describe('#save()', function() {
-    it('should save without error', function(done) {
-      var user = new User('Luna');
-      user.save(function(err) {
-        if (err) done(err);
-        else done();
-      });
+describe('msisdns', function () {
+    describe('#save()', function () {
+        it('should save without error', function (done) {
+
+            const text = 'INSERT INTO msisdns(id, email) VALUES($1, $2)';
+            const values = [Date.now(), 'mamunthecoder@gmail.com'];
+
+            client.query(text, values, function (err, res) {
+                if (err) {
+                    done(err.stack);
+                } else {
+                    done();
+                }
+            });
+        });
     });
-  });
 });
 
 
