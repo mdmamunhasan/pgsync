5d53465e758441e707d360423e33bed6a4ee62cc
mamun <mamunthecoder@gmail.com>
2017-08-13 21:02:18 +0600

    config stream updated

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var pg = require('pg');
 const Client = pg.Client;
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: config.aws_access_key,
+    secretAccessKey: config.aws_secret_key,
     region: "ap-southeast-1"
 });
 
